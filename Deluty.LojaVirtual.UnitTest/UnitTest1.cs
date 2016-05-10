7193018afcae0634e1610169373b53500c2cc110
Gilberto Antonio <gilbertoantonioti@outlook.com>
2016-05-09 22:13:14 -0300

    Commit

---
+++ Deluty.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Deluty.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestDeluty
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
